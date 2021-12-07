778059e9818040137c1e6507b22eae92b0a7cd08
Parker Erickson <parker.erickson@tigergraph.com>
2021-12-06 18:43:12 -0600

    update explore data

--- exploreData.py
+++ exploreData.py
@@ -1,41 +1,74 @@
 import dataset
 import numpy as np
-
-#data_sample = "2014-05-14-13-59-05"
-data_sample = "sample_small"
-
-ds = dataset.GraphCLIP(lidar_timestamp_path='./data/'+data_sample+'/lms_front.timestamps', 
-                        image_timestamp_path='./data/'+data_sample+'/stereo.timestamps',
-                        lidar_path='./data/'+data_sample+'/lms_front/',
-                        image_path='./data/'+data_sample+'/stereo/centre/',
-                        ins_path='./data/'+data_sample+'/gps/ins.csv',
-                        data_name=data_sample,
-                        use_cache=True)
-
-
-dataSample = 30
-print("Number of Image/Point Cloud Pairs:", len(ds))
-img = np.rot90(ds[dataSample]['image'].T, k=-1) # Reshape back to (H, W, C)
-
-
+import argparse
 import matplotlib.pyplot as plt
-plt.imshow(img)
-plt.show(block=False)
-
 import open3d
-
-pointcloud = ds[dataSample]["x"].numpy()
 from robotcar_dataset_sdk.python.transform import build_se3_transform
 
+def infer(model_path, dataset):
+    import torch
+    import CLIPGraphModel as cgm
+    modelConfig = model_path.split('/')[-1].split('.')[0]
+    model = cgm.CLIPGraphModel(image_model="resnet",
+                            graph_model=modelConfig.split('_')[0],
+                            embedding_dim=int(modelConfig.split('_')[-1]),
+                            graph_pool="mean",
+                            graph_hidden_dim=int(modelConfig.split('_')[2]),
+                            graph_out_dim=int(modelConfig.split('_')[4]),
+                            linear_proj_dropout=0.1,
+                            pretrained_image_model=bool(modelConfig.split('_')[-3]))
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=False)
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.eval()
+    similarities = []
+    for batch in dataloader:
+        print(batch)
+        loss, out = model(batch)
+        similarities += [np.diagonal(out.detach().numpy())]
+    return out
 
-vis = open3d.visualization.Visualizer()
-pcd = open3d.geometry.PointCloud()
-pcd.points = open3d.utility.Vector3dVector(pointcloud)
-pcd.transform(build_se3_transform([0, 0, 0, np.pi, 0, -np.pi / 2]))
-vis.create_window("CSCI 8980 Point Cloud Visualization")
-view_control = vis.get_view_control()
-params = view_control.convert_to_pinhole_camera_parameters()
-params.extrinsic = build_se3_transform([0, 3, 10, 0, -np.pi * 0.42, -np.pi / 2])
-view_control.convert_from_pinhole_camera_parameters(params)
-vis.add_geometry(pcd)
-vis.run()
+def main(args):
+    #data_sample = "2014-05-14-13-59-05"
+    data_sample = args.data_sample
+
+    ds = dataset.GraphCLIP(lidar_timestamp_path='./data/'+data_sample+'/lms_front.timestamps', 
+                            image_timestamp_path='./data/'+data_sample+'/stereo.timestamps',
+                            lidar_path='./data/'+data_sample+'/lms_front/',
+                            image_path='./data/'+data_sample+'/stereo/centre/',
+                            ins_path='./data/'+data_sample+'/gps/ins.csv',
+                            data_name=data_sample,
+                            use_cache=True)
+
+
+    data_index = args.data_index
+
+    print("Number of Image/Point Cloud Pairs:", len(ds))
+    if args.model_path is not None:
+        sim = infer(args.model_path, ds[data_index])
+        print("Similarity Between Embeddings:", sim)
+    img = np.rot90(ds[data_index]['image'].T, k=-1) # Reshape back to (H, W, C)
+
+    plt.imshow(img)
+    plt.show(block=False)
+
+    pointcloud = ds[data_index]["x"].numpy()
+
+    vis = open3d.visualization.Visualizer()
+    pcd = open3d.geometry.PointCloud()
+    pcd.points = open3d.utility.Vector3dVector(pointcloud)
+    pcd.transform(build_se3_transform([0, 0, 0, np.pi, 0, -np.pi / 2]))
+    vis.create_window("CSCI 8980 Point Cloud Visualization")
+    view_control = vis.get_view_control()
+    params = view_control.convert_to_pinhole_camera_parameters()
+    params.extrinsic = build_se3_transform([0, 3, 10, 0, -np.pi * 0.42, -np.pi / 2])
+    view_control.convert_from_pinhole_camera_parameters(params)
+    vis.add_geometry(pcd)
+    vis.run()
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--data_sample", "-ds", type=str, default="sample")
+    args.add_argument("--data_index", "-di", type=int, default=0)
+    args.add_argument("--model_path", "-mp", type=str, default=None)
+    args = args.parse_args()
+    main(args)
