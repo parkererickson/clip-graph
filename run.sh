c8eb5f083998d38bf96f7e67e2c6cee0c7c3a027
Parker Erickson <parker.erickson30@gmail.com>
2021-12-16 09:27:48 -0600

    updated models

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-python main.py -bs 64 -gm gcn & wait;
-python main.py -bs 64 -gm gat & wait;
-python main.py -bs 32 -gm gat & wait;
-python main.py -bs 64 -pim False & wait;
-python main.py -bs 64 -pim False -gm gcn & wait;
-python main.py -bs 64 -pim False -gm gat & wait;
-python main.py -bs 32 -pim False -gm gat
+python main.py -bs 32 -gm gat -gp mean & wait;
+python main.py -bs 32 -gm gat -gp max & wait;
+python main.py -bs 32 -pim False -gm gat -gp max & wait;
